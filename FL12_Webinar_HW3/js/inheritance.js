bfc07916ae2ea15da3ce365c68cfed08eedc69e5
Vladyslav Shypko <58210005+VladyslavShypko@users.noreply.github.com>
2020-02-24 23:25:10 +0300

    some fixes

--- FL12_Webinar_HW3/js/inheritance.js
+++ FL12_Webinar_HW3/js/inheritance.js
@@ -1,18 +1,4 @@
-const checkBenefits = (obj, str) => {
-		for (let i in obj) {
-			if (i === 'salary') {
-				console.log(str);
-				this.changeSalary(obj[i]);
-			} else if (i === 'position') {
-				console.log(str);
-				this.changePosition(obj[i]);
-			} else if (i === 'department') {
-				console.log(str);
-				this.changeDepartment(obj[i]);
-			}
-		}
-};
-
+// task 2
 class Employee {
 	constructor(obj) {
 		this.id = obj.id;
@@ -26,20 +12,19 @@ class Employee {
 	}
 	get age() {
 		let arr = this.birthday.split('/').reverse();
-		return Math.floor( ( (new Date - new Date(arr[0], arr[1]-1, arr[2])) / 1000 / (60 * 60 * 24) ) / 365.25 );
+		return Math.floor(((new Date - new Date(arr[0], arr[1] - 1, arr[2])) / 1000 / (60 * 60 * 24)) / 365.25);
 	}
-    get fullName() {
+	get fullName() {
 		return `${this.firstName} ${this.lastName}`;
 	}
 
-    static _employees = [];
-    static get EMPLOYEES() {
-		return this._employees;
+	static _employees = [];
+	static get EMPLOYEES() {
+		return Employee._employees;
 	}
 	quit = () => {
-		let idIndex = Employee._employees.indexOf(this);
-		if (idIndex >= 0) {
-			Employee._employees.splice(idIndex, 1);
+		if (Employee._employees.indexOf(this) >= 0) {
+			Employee._employees.splice(Employee._employees.indexOf(this), 1);
 		}
 	}
 	retire = () => {
@@ -59,20 +44,35 @@ class Employee {
 	changeSalary = (newSalary) => {
 		this.salary = newSalary;
 	}
+	checkAction = (obj, str) => {
+		for (let i in obj) {
+			if (i === 'salary') {
+				this.changeSalary(obj[i]);
+				console.log(str);
+			} else if (i === 'position') {
+				this.changePosition(obj[i]);
+				console.log(str);
+			} else if (i === 'department') {
+				this.changeDepartment(obj[i]);
+				console.log(str);
+			}
+		}
+	}
 	getPromoted = (benefits) => {
-		checkBenefits(benefits, 'Yahooo!');
+		this.checkAction(benefits, 'Yahooo!');
 	}
 	getDemoted = (punishment) => {
-		checkBenefits(punishment, 'Damn!');
+		this.checkAction (punishment, 'Damn!');
+	}
 }
 
 class Manager extends Employee {
 	constructor(obj) {
 		super(obj);
-		this.position  = 'manager';
+		this.position = 'manager';
 	}
 	get managedEmployees() {
-		 return Employee.employees.filter(emp => emp.department === this.department && emp.position !== 'manager');
+		return Employee._employees.filter((emp) => emp.department === this.department && emp.position !== 'manager');
 	}
 }
 
@@ -92,6 +92,64 @@ class SalesManager extends Manager {
 	}
 }
 
+// task 3
 
-let employee1 = new Employee({id: 1, firstName: 'Vlad', lastName: 'Shypko', birthday: '25/07/1996', salary: 500, position: 'worker', department: 'worker'});
-let employee2 = new Employee({id: 2, firstName: 'Vlad', lastName: 'Shypko', birthday: '29/07/1996', salary: 500, position: 'worker', department: 'worker'});
+const promoter = (manager) => ({
+	promote(indexEmployees, newSalary) {
+		manager.managedEmployees.find((emp) => emp.id === indexEmployees).getPromoted({salary: newSalary})
+	}
+});
+function ManagerPro(manager, action) {
+	if (manager instanceof Manager) {
+		return Object.assign(manager, action(manager));
+	}
+}
+
+// Run example
+
+const salesManager = new SalesManager({
+	id: 1,
+	firstName: 'John',
+	lastName: 'Doe',
+	birthday: '10/04/1994',
+	salary: 5000
+});
+const hrManager = new HRManager({
+	id: 2,
+	firstName: 'Bob',
+	lastName: 'Doe',
+	birthday: '10/04/1994',
+	salary: 5000
+});
+const blueCollarWorkerOne = new BlueCollarWorker({
+	id: 3,
+	firstName: 'Mary',
+	lastName: 'Doe',
+	birthday: '29/07/1996',
+	salary: 5000,
+	position: 'office worker',
+	department: 'sales'
+});
+const blueCollarWorkerTwo = new BlueCollarWorker({
+	id: 4,
+	firstName: 'Jane',
+	lastName: 'Doe',
+	birthday: '29/07/1996',
+	salary: 5000,
+	position: 'office worker',
+	department: 'hr'
+});
+
+console.log(Employee.EMPLOYEES);
+console.log(salesManager.getPromoted({
+	salary: 7500
+}));
+console.log(salesManager.salary);
+console.log(blueCollarWorkerTwo.birthday);
+console.log(blueCollarWorkerTwo.fullName);
+console.log(blueCollarWorkerTwo.age);
+const managerPro = ManagerPro(salesManager, promoter);
+managerPro.promote(3, 6000);
+console.log(blueCollarWorkerOne.salary);
+blueCollarWorkerTwo.getFired();
+console.log(Employee.EMPLOYEES);
